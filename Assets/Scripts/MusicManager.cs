ff07520a17b6ac45e8c4f2a6d0aa7896ad1d3102
vincent <pk0119@gmail.com>
2016-07-05 00:07:32 -0700

    Music stuffs. 1. Add a persistent music manager that can play different music on different scene. 2. Add some sound clips

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] audioClips;
+	private AudioSource music;
+
+	void Awake() {
+		// Don't destroy this gameObject when a new scene is loaded.
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start() {
+		music = gameObject.GetComponent<AudioSource>();
+	}
+
+	void OnLevelWasLoaded(int level) {
+		// Since the first scene will never invoke this function, level 0 will never be passed into here
+		if (level < 0 || level >= audioClips.Length) {
+			Debug.LogError("Level index out of range");
+			return;
+		}
+		AudioClip thisLevelMusic = audioClips[level];
+		if(thisLevelMusic) {
+			music.clip = thisLevelMusic;
+			music.loop = true;
+			music.Play();
+		+
+	}
+}
